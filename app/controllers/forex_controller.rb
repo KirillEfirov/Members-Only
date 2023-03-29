18b11b442f5546da58295b918cd09030a8f5a77f
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-03-29 09:32:46 +0300

    Make #convert method

--- app/controllers/forex_controller.rb
+++ app/controllers/forex_controller.rb
@@ -9,7 +9,28 @@ class ForexController < ApplicationController
     pair = params[:pairs]
     response = get_http_response("https://www.freeforexapi.com/api/live?pairs=#{pair}")
 
-    render json: response.body
+    json = JSON.parse(response.body)
+
+    if json['rates'].nil?
+      render json: "Add parameters\n FORMAT: ?pairs=EURUSD"
+    else
+      render json: response.body
+    end
+  end
+
+  def convert
+    pair = params[:pairs]
+
+    response = get_http_response("https://www.freeforexapi.com/api/live?pairs=#{pair}")
+
+    json = JSON.parse(response.body)
+
+    amount = params[:amount]
+    currency_rate = json['rates']["#{pair}"]['rate']
+
+    conversion = convert_currency(amount, currency_rate)
+
+    render json: conversion
   end
 
   private
@@ -20,24 +41,8 @@ class ForexController < ApplicationController
     http.use_ssl = true
     http.get(url)
   end
-end
 
-
-=begin
-  def show_pairs
-    pairs = params[:pairs]
-    url = URI("https://www.freeforexapi.com/api/live?pairs=#{pairs}")
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
-    response = http.get(url)
-
-    json = JSON.parse(response.body)
-    rate = json['rates']["#{pairs}"]['rate']
-
-    how_much = params[:how_much]
-
-    exchange = how_much.to_f * rate.to_f
-
-    render json: exchange
+  def convert_currency(amount, currency_rate)
+    conversion = amount.to_f * currency_rate.to_f
   end
-=end
+end
