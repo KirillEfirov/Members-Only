c291e62e47e284a3b5195b1955e74dfbbf75d803
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-03-29 05:45:14 +0300

    Add path to forex controller with index action that shoul list all the available currency pairs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  root to: "posts#index"
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "posts#index"
 
   resources :posts, only: [:new, :create, :index]
+
+  get '/forex', to: 'forex#index'
+
+  
+  #get '/forex', to: 'forex#index'
+  #get '/forex/pairs/:pairs/:how_much', to: 'forex#show_pairs'
 end
