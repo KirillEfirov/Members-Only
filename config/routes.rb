750f2ccbfe2ed314ffcf42022e611134c81a8b34
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-03-29 06:03:21 +0300

    Add path to retrieve one currency pair

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  root to: "posts#index"
-  
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-
+  root to: "posts#index"
   resources :posts, only: [:new, :create, :index]
 
   get '/forex', to: 'forex#index'
-
-  
-  #get '/forex', to: 'forex#index'
+  get '/forex/:pair', to: 'forex#get_currency_pair'
   #get '/forex/pairs/:pairs/:how_much', to: 'forex#show_pairs'
 end
