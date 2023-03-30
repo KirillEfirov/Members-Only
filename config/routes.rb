59597b473cf1af5882d66cbc654c5f931b071c7f
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-03-30 16:35:41 +0300

    Change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "posts#index"
+  root to: "forex#index"
   resources :posts, only: [:new, :create, :index]
 
   get '/forex', to: 'forex#index'
-  get '/forex/pair', to: 'forex#get_currency_pair'
+  get '/forex/pair', to: 'forex#show'
   get '/forex/convert', to: 'forex#convert'
 end
