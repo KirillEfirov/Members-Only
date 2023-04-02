e02e4b40c86c42002908beba38880de9b557a1ae
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-04-03 01:41:18 +0300

    Bring back data as a hash

--- app/services/forex_converter.rb
+++ app/services/forex_converter.rb
@@ -7,13 +7,12 @@ class ForexConverter
   
   def convert
     pair = @from + @to
+    currency = ForexPair.new(pair).get_currency
 
-    currency = ForexPair.new(pair).get_currency(pair)
-
-    if currency == ForexPair::ERROR_MESSAGE
-      "Can't convert from #{@from} to #{@to}"
+    if(currency == currency[:error] || currency[:currency].nil?)
+      { error: "Can't convert from #{@from} to #{@to}" }
     else
-      "#{@amount} #{@from} = #{convert_currency(@amount, currency.rate)} #{@to}"
+      { conversion: convert_currency(@amount, currency[:currency].rate) }
     end
   end
   
