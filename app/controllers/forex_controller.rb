af97b084bc6f4046ed984cae271f3c8a70a4aaca
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-04-06 13:28:39 +0300

    Adjusted the controller to the changes in the service objects

--- app/controllers/forex_controller.rb
+++ app/controllers/forex_controller.rb
@@ -5,24 +5,14 @@ class ForexController < ApplicationController
 
   def show
     @pair = params[:pairs]
-    currency_data = fetch_currency_data(@pair)
+    currency_data = ForexPair.call(@pair)
 
     currency_data[:error] ? flash[:error] = currency_data[:error] : @currency = currency_data[:currency]
   end
 
   def convert
-    conversion = fetch_conversion_data(params[:from], params[:to], params[:amount])
+    conversion = ForexConverter.call(params[:from], params[:to], params[:amount])
 
     conversion[:error] ? flash[:error] = conversion[:error] : @conversion = conversion[:conversion]
-  end
-
-  private
-
-  def fetch_currency_data(pair)
-    ForexPair.new(pair).get_currency
-  end
-
-  def fetch_conversion_data(from, to, amount)
-    ForexConverter.new(from, to, amount).convert
   end
 end
