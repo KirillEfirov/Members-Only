efcb34e3fb28148094f435b1aec8e0ee7d382a68
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-04-06 13:25:19 +0300

    Refactor code & get rid of to_f option in rate in order converter return value without inaccuracy

--- app/services/forex_converter.rb
+++ app/services/forex_converter.rb
@@ -1,27 +1,24 @@
+# frozen_string_literal: true
+
 class ForexConverter
-  ERROR_MESSAGE = "Can't convert currency"
+  ERROR_MESSAGE = 'Can\'t convert currency'
 
-  def initialize(from, to, amount)
-    @from = from
-    @to = to
-    @amount = amount
-  end
-  
-  def convert
-    pair = @from.to_s + @to.to_s
-    currency = ForexPair.new(pair).get_currency
+  class << self
+    def call(from, to, amount)
+      pair = [from, to].join
+      currency = ForexPair.call(pair)
 
-    if(currency == currency[:error] || currency[:currency].nil?)
-      { error: ForexConverter::ERROR_MESSAGE }
-    else
-      { conversion: convert_currency(@amount, currency[:currency].rate) }
+      if currency == currency[:error] || currency[:currency].nil?
+        { error: ForexConverter::ERROR_MESSAGE }
+      else
+        { conversion: convert_currency(amount, currency[:currency].rate) }
+      end
+    end
+
+    private
+
+    def convert_currency(amount, currency_rate)
+      amount.to_f * currency_rate
     end
   end
-  
-  private
-  
-  def convert_currency(amount, currency_rate)
-    amount.to_f * currency_rate.to_f
-  end
 end
-  
